8754b5c0f1932059b561854076460fbebe4afb7f
Adarsh Raj <ahazadarsh0014@gmail.com>
2022-10-28 09:58:56 +0530

    pg addded error solved

--- Routes/CashInOutRoute.js
+++ Routes/CashInOutRoute.js
@@ -1,7 +1,8 @@
 import express from "express";
+import { cashIn, cashOut } from "../Controllers/CashInOutController.js";
 
 const router = express.Router();
 
-router.post("/payment")
-router.post("/payout")
+router.post("/payment",cashIn)
+router.post("/payout",cashOut)
 export default router;
